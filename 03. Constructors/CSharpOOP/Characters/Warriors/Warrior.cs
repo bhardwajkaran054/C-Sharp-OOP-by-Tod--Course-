deb53617c350f10409fe7adfeb232569de216f98
Karan Bhardwaj <bhardwajkaran054@gmail.com>
2020-06-09 17:33:50 +0530

    Add files via upload

---
+++ 03. Constructors/CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Weapons;
+
+namespace Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private int age;
+        private string name;
+        private Sword swordWeapon;
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 45)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"The selected age for {Name}, is not a proper age for a Warrior. Please use age between 18 and 45.");
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Sword SwordWeapon
+        {
+            get
+            {
+                return this.swordWeapon;
+            }
+            set
+            {
+                this.swordWeapon = value;
+            }
+        }
+
+        public Warrior()
+            : this(170, 70)
+        {
+        }
+
+        public Warrior(int height, int weight)
+            : this(height, weight, "Young Warrior")
+        {
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            this.Height = height;
+            this.Weight = weight;
+            this.Name = name;
+            this.Age = 18;
+            this.SwordWeapon = new Sword();
+        }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($"Greetings {warrior.Name}!");
+        }
+    }
+}
---
+++ 03. Constructors/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using Characters.Warriors;
+using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior firstWarrior = new Warrior();
+        Warrior secondWarrior = new Warrior(150, 50);
+        Warrior thirdWarrior = new Warrior(140, 78, "I am the third warrior!");
+
+        Console.WriteLine(firstWarrior.Name);
+        Console.WriteLine(thirdWarrior.Name);
+    }
+}
