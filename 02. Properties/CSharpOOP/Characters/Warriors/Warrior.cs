f9f0a36297a310a67e995328e04eb3f014ed008f
Karan Bhardwaj <bhardwajkaran054@gmail.com>
2020-06-09 16:49:31 +0530

    Add files via upload

---
+++ 02. Properties/CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private int age;
+        private string name;
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 45)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"The selected age for {Name}, is not a proper age for a Warrior. Please use age between 18 and 45.");
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Warrior(int height, int weight, string name)
+        {
+            this.Height = height;
+            this.Weight = weight;
+            this.Name = name;
+        }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($"Greetings {warrior.Name}!");
+        }
+    }
+}
---
+++ 02. Properties/CSharpOOP/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using Characters.Warriors;
+using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior(190, 80, "Good Guy");
+        Warrior theBadGuy = new Warrior(170, 80, "Bad Guy");
+
+        Console.WriteLine(theBadGuy.Height);
+        Console.WriteLine(theGoodGuy.Height);
+
+        try
+        {
+            theGoodGuy.Age = 25;
+            theBadGuy.Age = 15;
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        theGoodGuy.Greetings(theBadGuy);
+        theBadGuy.Greetings(theGoodGuy);
+    }
+}
